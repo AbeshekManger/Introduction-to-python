f3ac2cc7a8694eb320ef92369d3e417774a659fb
Abeshek Manger <abeshek.manger@msu.edu.in>
2023-04-19 04:24:24 +0530

    pushing

---
+++ rana.py
@@ -0,0 +1,7 @@
+pi= 22/7
+height = float(input('height of the cylinder: '))
+radian = float(input('radius of sylinder: '))
+volume = pi *radian* radian*height 
+sur_area =((2*pi*radian)*height)+((pi*radian**2)*2)
+print("volume is :", volume)
+ 
---
+++ width = float (input('enter the width of.py
@@ -0,0 +1,4 @@
+width = float (input('enter the width of the rectangle:'))
+height = float (input ('enter the height of the rectangle'))
+area = width*height
+print("areaof a rectangle is: %.2f" %area)
